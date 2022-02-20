cb58d35e943fa802640f6dc0a19c282b5a122aa3
Ivan Annan <primeguyy@gmail.com>
2022-02-20 18:02:39 +0000

    template routes added

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/goals", (req, res) => {
-  res.status(200).json({ success: true, message: "My Goals..." });
+router.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: `Get Goals...` });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ success: true, message: `Add Goals...` });
+});
+
+router.put("/:goalID", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update Goal ${req.params.goalID}...` });
+});
+
+router.delete("/:goalID", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete Goal ${req.params.goalID}...` });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const portNumber = process.env.PORT;
-const router = require("./routes/goalRoutes");
+const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
 
-app.use(router);
+app.use("/api/goals", goalRoutes);
 
 app.listen(portNumber, () =>
   console.log(`Server started on http://localhost:${portNumber}`)
