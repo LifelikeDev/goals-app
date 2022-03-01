b8c3534f2b277505810949ba16500861a12bd54b
Ivan Annan <primeguyy@gmail.com>
2022-03-01 21:32:09 +0000

    register user added

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,27 +1,63 @@
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
-const asyncHandler = require("express-async-handler")
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 // @desc Register User
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
-  res.json({ message: "User registered" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all required fields...");
+  }
+
+  // check if user exists
+  const userPresent = await User.findOne({ email });
+
+  if (userPresent) {
+    res.status(400)
+    throw new Error(`${name} already exists...`);
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401)
+    throw new Error("Error creating user: Invalid user data...");
+  }
+});
 
 // @desc Authenticate User
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "User logged in" });
-};
+});
 
 // @desc Get User Data
 // @route GET /api/users/current
 // @access Public
-const getCurrentUser = (req, res) => {
+const getCurrentUser = asyncHandler(async (req, res) => {
   res.json({ message: "Current User Data" });
-};
+});
 
 module.exports = {
   registerUser,
