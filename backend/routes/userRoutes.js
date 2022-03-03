1187b2a8d40228deee27ef1be827b28de7dcd04b
Ivan Annan <primeguyy@gmail.com>
2022-03-03 23:01:55 +0000

    protect authMiddleware applied in userRoute

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, getCurrentUser } = require("../controllers/usersController");
 
+const {protect} = require("../middleware/authMiddleware")
+
 router.post("/", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", getCurrentUser)
+router.get("/current", protect, getCurrentUser)
 
 module.exports = router;
