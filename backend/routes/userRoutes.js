7da32b44b8e2c1c2fce7c2ba90df574574fd8c62
Ivan Annan <primeguyy@gmail.com>
2022-03-05 22:53:38 +0000

    changed style of export of protect function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getCurrentUser } = require("../controllers/usersController");
+const {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+} = require("../controllers/usersController");
 
-const {protect} = require("../middleware/authMiddleware")
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", protect, getCurrentUser)
+router.get("/current", protect, getCurrentUser);
 
 module.exports = router;
