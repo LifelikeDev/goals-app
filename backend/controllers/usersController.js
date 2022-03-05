96989dfc98feb0d1ef34e3e8bcc433c7ac335aa5
Ivan Annan <primeguyy@gmail.com>
2022-03-05 23:08:34 +0000

    usersController.js updated

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -73,7 +73,15 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/current
 // @access Private
 const getCurrentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current User Data" });
+  const {_id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+    message: `${email} successfully signed in`
+  })
+
 });
 
 // generate token
