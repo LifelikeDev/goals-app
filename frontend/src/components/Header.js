327a0037a73de4c1f69c0be110759f09ef1489ec
lifelikeDev <primeguyy@gmail.com>
2022-03-31 23:04:59 +0000

    login methods defined

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,6 @@ const Header = () => {
   const handleLogout = () => {
     dispatch(logout());
     dispatch(reset());
-
     navigate("/");
   };
 
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -21,7 +21,18 @@ export const register = createAsyncThunk("auth/register", async(user, thunkAPI) 
     }
 })
 
-// Log User out
+
+// Log User In
+export const login = createAsyncThunk("auth/login", async(user, thunkAPI) => {
+    try {
+        return await authService.login(user)
+    } catch(error) {
+        const message = (error.message && error.message.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+// Log User Out
 export const logout = createAsyncThunk("auth/logout", async() => {
   await authService.logout();
 })
@@ -52,6 +63,22 @@ export const authSlice = createSlice({
               state.isLoading = false;
               state.isSuccess = true;
               state.message = action.payload
+          })
+          .addCase(login.pending, (state) => {
+              state.isLoading = true;
+          })
+          .addCase(login.fulfilled, (state, action) => {
+              state.isLoading = false;
+              state.isSuccess = true
+              state.user = action.payload
+          })
+          .addCase(login.rejected, (state, action) => {
+              state.isLoading = false;
+              state.isSuccess = true;
+              state.message = action.payload
+          })
+          .addCase(logout.fulfilled, (state) => {
+              state.user = null
           })
    },
 });
