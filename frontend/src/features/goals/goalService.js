687b31d496bbf2fa6b7de1a27c975a037fe80609
lifelikeDev <primeguyy@gmail.com>
2022-04-02 15:06:07 +0000

    add goal functionality added

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,8 +1,19 @@
+import axios from "axios";
+
+const API_URL = "/api/goals/";
+
+// Add new goal
+const addGoal = async (goalData, token) => {
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    }
+    
+    const response = await axios.post(API_URL, goalData, config);
+
+    return response.data;
+}
 
 
+const goalService = { addGoal }
 
-
-
-
-
-// export default goalService;
+export default goalService;
--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import goalService from "./goalService";
+import goalService from "./goalService";
 
 const initialState = {
     goals: [],
@@ -10,7 +10,15 @@ const initialState = {
 }
 
 // add goal
-export const addGoal = () => {}
+export const addGoal = createAsyncThunk("goals/addGoal", async (goalData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await goalService.addGoal(goalData, token);
+    } catch(error) {
+        const message = (error.message && error.message.data && error.response.data.message) || error.message || error.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+})
 
 
 export const goalSlice = createSlice({
@@ -18,6 +26,22 @@ export const goalSlice = createSlice({
     initialState,
     reducers: {
         reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(addGoal.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(addGoal.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.goals.push(action.payload);
+            })
+            .addCase(addGoal.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload
+            })
     }
 })
 
